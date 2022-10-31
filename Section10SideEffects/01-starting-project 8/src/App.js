155ee5c7f5ca281fd74c0942bfc51c82ad3715f6
mrylraihan <mr.ylraihan@gmail.com>
2022-10-31 15:40:10 -0700

    upskilling side effects react useEffect

--- Section10SideEffects/01-starting-project 8/src/App.js
+++ Section10SideEffects/01-starting-project 8/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,26 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(()=>{
+    // pull a key from the localStorage 
+    const storedUserLoginInfo = localStorage.getItem('isLoggedIn')
+    if(storedUserLoginInfo === '1'){
+      setIsLoggedIn(true)
+    }
+  },[])
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    // when you using setItem you creating a object/ key value pair
+    // the first parameter will be the key and the second will be the value
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+
+    // clear method will clear all store objects in localStorage
+    localStorage.clear()
     setIsLoggedIn(false);
   };
 
