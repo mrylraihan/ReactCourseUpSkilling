dfc17188520d56c4cfa8314ce5ddf631f3b8195c
mrylraihan <mr.ylraihan@gmail.com>
2022-12-07 10:20:51 -0800

    just incase

--- Section10SideEffectsPart4(NewUseContextp2)/06-usereducer-starting-project/src/store/auth-context.js
+++ Section10SideEffectsPart4(NewUseContextp2)/06-usereducer-starting-project/src/store/auth-context.js
@@ -18,6 +18,7 @@ export const AuthContextProvider = (props)=>{
             setIsLoggedIn(true);
         }
     }, []);
+    
     const loginHandler = (email, password) => {
         // We should of course check email and password
         // But it's just a dummy/ demo anyways
