16aa778aa150b462c58b61a2463d5e3350edaaaa
mrylraihan <mr.ylraihan@gmail.com>
2022-12-24 10:55:04 -0800

    custom hook practice

--- Section15CustomHooks(Part3)/04-onwards-to-a-more-realistic-example 2/src/App.js
+++ Section15CustomHooks(Part3)/04-onwards-to-a-more-realistic-example 2/src/App.js
@@ -2,41 +2,24 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
-
+import useHttp from './hooks/use-http';
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const httpData = useHttp()
+  const {isLoading, error, sendRequest : fetchTasks} = httpData
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  useEffect(() => {
+    const transformTask = taskObj => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
     }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
+    
+    fetchTasks({ url: 'https://react-http2-3c478-default-rtdb.firebaseio.com/tasks.json' }, transformTask);
   }, []);
 
   const taskAddHandler = (task) => {
--- Section15CustomHooks(Part3)/04-onwards-to-a-more-realistic-example 2/src/components/NewTask/TaskForm.js
+++ Section15CustomHooks(Part3)/04-onwards-to-a-more-realistic-example 2/src/components/NewTask/TaskForm.js
@@ -13,6 +13,7 @@ const TaskForm = (props) => {
     if (enteredValue.trim().length > 0) {
       props.onEnterTask(enteredValue);
     }
+    taskInputRef.current.value = ''
   };
 
   return (
