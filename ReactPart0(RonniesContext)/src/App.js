30c8460f47986dfea10eaf0beebcf10e6905ae68
mrylraihan <mr.ylraihan@gmail.com>
2023-01-31 13:59:58 -0500

    custom hook practice

--- ReactPart0(RonniesContext)/src/App.js
+++ ReactPart0(RonniesContext)/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import './App.css';
+import { DataProvider } from './context/dataContext';
+import HomePage from './Pages/HomePage';
 
 
 function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DataProvider>
+      <HomePage/>
+      </DataProvider>
     </div>
   );
 }
