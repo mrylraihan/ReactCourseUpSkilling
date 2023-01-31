eecbb947beec0445f12a3e9b8755544e06e17f0d
mrylraihan <mr.ylraihan@gmail.com>
2023-01-31 11:18:03 -0500

    customhook practice toggle

--- ReactPart0(RonniesContext)/src/App.js
+++ ReactPart0(RonniesContext)/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 import './App.css';
-import TestCustomHook from './components/TestCustomHook';
 
 
 function App() {
   return (
-    <>
     <div className="app">
       <h1>Hi there!</h1>
     </div>
-    <TestCustomHook/>
-    </>
   );
 }
 
