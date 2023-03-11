e963af2c151bcdbaab6f0007544f64aceda98b68
mrylraihan <mr.ylraihan@gmail.com>
2023-03-11 13:15:02 -0500

    forms validation and onblur practice

---
+++ ReactPart0(FormsValidation)/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import FormValidation from './components/FormValidation';
+import FormValidation2 from './components/FormValidation2';
+import FormValidation3 from './components/FormValidation3';
+
+
+
+function App() {
+  return (
+    <>
+    {/* <div className="app">
+      <FormValidation/>
+    </div>    
+    <div className="app">
+      <FormValidation2/>
+    </div>     */}
+    <div className='app'>
+    <FormValidation3/>
+    </div>
+    </>
+  );
+}
+
+export default App;
