bec1909f1aca9bd6fff554308002f1258668f007
mrylraihan <mr.ylraihan@gmail.com>
2023-03-27 12:48:50 -0700

    redux toolkit

---
+++ ReactPart0(ReduxToolkitBasic4)/src/store/index.js
@@ -0,0 +1,6 @@
+import {configureStore} from '@reduxjs/toolkit';
+import nameReducers from './FullNameSlice'
+import toggleReducer from './ToggleSlice'
+const store = configureStore({reducer:{name:nameReducers, toggle:toggleReducer}})
+
+export default store
