f2722867227cd0f37c82b77692c45c4ec32a6cda
mrylraihan <mr.ylraihan@gmail.com>
2023-05-08 12:18:06 -0400

    useLoaderData practice

--- ReactPart0(Routers5BasicCreateRoutesFromElementLoaderPractice)/src/App.js
+++ ReactPart0(Routers5BasicCreateRoutesFromElementLoaderPractice)/src/App.js
@@ -1,18 +1,18 @@
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import './App.css';
 import Home from './components/Pages/Home';
-import All from './components/Pages/All';
-import People from './components/Pages/People';
+import All, { peopleLoader } from './components/Pages/All';
+import People, { onePersonLoader }from './components/Pages/People';
 import More from './components/Pages/More';
 import About from './components/Pages/About';
 import RootLayout from './components/Pages/RootLayout';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<RootLayout />}>
-    <Route index element={<Home/>}/>
+    <Route index element={<Home />} />
     <Route path='all'>
-    <Route index element={<All/>}/>
-    <Route path=':id' element={<People/>}/>
+      <Route index element={<All />} loader={peopleLoader} />
+      <Route path=':id' element={<People />} loader={onePersonLoader}/>
     </Route>
     <Route path='extra'>
     <Route index element={<More/>}/>
